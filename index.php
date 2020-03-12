8a13357787866a735656523bce0c31b895d10ec2
SlavChek <vs.litvinov@gmail.com>
2020-03-13 02:02:47 +0400

    Implemented showing lots and lower menu using php Реализован показ лотов и нижнего меню с помощью php

--- index.php
+++ index.php
@@ -3,6 +3,35 @@ $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+
+$categories = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+
+$products = [
+    ['title' => '2014 Rossignol Distroct snowboard',
+     'category' => $categories[0],
+     'cost' => 10999,
+     'url' => 'img/lot-1.jpg'],
+     ['title' => 'DC Ply Mens 2016/2017 Snowboard',
+      'category' => $categories[0],
+      'cost' => 159999,
+      'url' => 'img/lot-2.jpg'],
+     ['title' => 'Крепления Union Contact Pro 2015 года, размер L/XL',
+      'category' => $categories[1],
+      'cost' => 8000,
+      'url' => 'img/lot-3.jpg'],
+     ['title' => 'Ботинки для сноуборда DC Mutiny Charocal',
+      'category' => $categories[2],
+      'cost' => 10999,
+      'url' => 'img/lot-4.jpg'],
+     ['title' => 'Куртка для сноуборда DC Mutiny Charocal',
+      'category' => $categories[3],
+      'cost' => 7500,
+      'url' => 'img/lot-5.jpg'],
+     ['title' => 'Маска Oakley Canopy',
+      'category' => $categories[5],
+      'cost' => 5400,
+      'url' => 'img/lot-6.jpg']
+   ];
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -25,7 +54,7 @@ $user_avatar = 'img/user.jpg';
                 </form>
                 <a class="main-header__add-lot button" href="add-lot.html">Добавить лот</a>
                 <nav class="user-menu">
-                    <!-- Реализован показ кода HTML кода по условию if else -->
+                    <!-- Реализован показ HTML кода по условию if else -->
                     <?php if ($is_auth == true): ?>
                     <div class="user-menu__image">
                         <img src="<?= $user_avatar?>" width="40" height="40" alt="User photo">
@@ -44,7 +73,7 @@ $user_avatar = 'img/user.jpg';
                         </li>
                     </ul>
                     <?php endif ;?>
-                    <!-- Реализован показ кода HTML кода по условию if else -->
+                    <!-- Реализован показ HTML кода по условию if else -->
                 </nav>
             </div>
         </header>
@@ -78,49 +107,45 @@ $user_avatar = 'img/user.jpg';
                     <h2>Открытые лоты</h2>
                 </div>
                 <ul class="lots__list">
+                    <!--Реализация показа лотов с помощью цикла foreach и двумерного асоциативного массива-->
+                    <?php foreach ($products as $key => $value): ?>
                     <li class="lots__item lot">
                         <div class="lot__image">
-                            <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                            <img src="<?=$value['url']?>" width="350" height="260" alt="Сноуборд">
                         </div>
                         <div class="lot__info">
-                            <span class="lot__category">Доски и лыжи</span>
+                            <span class="lot__category"><?=$value['category']?></span>
                             <h3 class="lot__title">
-                                <a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a>
+                                <a class="text-link" href="lot.html"><?=$value['title']?></a>
                             </h3>
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost">10 999<b class="rub">р</b>
+                                    <span class="lot__cost"><?=$value['cost']?>
+                                        <b class="rub">р</b>
                                     </span>
                                 </div>
                                 <div class="lot__timer timer"></div>
                             </div>
                         </div>
                     </li>
+                    <?php endforeach?>
+                    <!--Реализация показа лотов с помощью цикла foreach и двумерного асоциативного массива-->
                 </ul>
             </section>
         </main>
         <footer class="main-footer">
             <nav class="nav">
                 <ul class="nav__list container">
+                    <!-- Выводим список категорий с помощью цикла foreach -->
+                    <?php foreach ($categories as $value): ?>
                     <li class="nav__item">
-                        <a href="all-lots.html">Доски и лыжи</a>
+                        <a href="all-lots.html"><?=$value;?></a>
                     </li>
-                    <li class="nav__item">
-                        <a href="all-lots.html">Крепления</a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="all-lots.html">Ботинки</a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="all-lots.html">Одежда</a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="all-lots.html">Инструменты</a>
-                    </li>
-                    <li class="nav__item">
-                        <a href="all-lots.html">Разное</a>
-                    </li>
+                    <!-- <li class="nav__item"> <a href="all-lots.html">Крепления</a> </li> <li class="nav__item"> <a href="all-lots.html">Ботинки</a> </li> <li class="nav__item"> <a href="all-lots.html">Одежда</a> </li> <li class="nav__item"> <a
+                    href="all-lots.html">Инструменты</a> </li> <li class="nav__item"> <a href="all-lots.html">Разное</a> </li> -->
+                    <!-- Выводим список категорий с помощью цикла foreach -->
+                    <?php endforeach; ?>
                 </ul>
             </nav>
             <div class="main-footer__bottom container">
