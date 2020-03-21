a423e56980ce127d25ccc47d99ada311e39f762c
SlavChek <vs.litvinov@gmail.com>
2020-03-21 12:14:52 +0400

    Правки

--- templates/index.php
+++ templates/index.php
@@ -8,21 +8,9 @@
                 <a class="promo__link" href="all-lots.html"><?= $value['title']?></a>
             </li>
             <?php endforeach;?>
-            <!--<li class="promo__item promo__item--attachment">
-                <a class="promo__link" href="all-lots.html">Крепления</a>
-            </li>
-            <li class="promo__item promo__item--boots">
-                <a class="promo__link" href="all-lots.html">Ботинки</a>
-            </li>
-            <li class="promo__item promo__item--clothing">
-                <a class="promo__link" href="all-lots.html">Одежда</a>
-            </li>
-            <li class="promo__item promo__item--tools">
-                <a class="promo__link" href="all-lots.html">Инструменты</a>
-            </li>
-            <li class="promo__item promo__item--other">
-                <a class="promo__link" href="all-lots.html">Разное</a>
-            </li>-->
+            <!--<li class="promo__item promo__item--attachment"> <a class="promo__link" href="all-lots.html">Крепления</a> </li> <li class="promo__item promo__item--boots"> <a class="promo__link" href="all-lots.html">Ботинки</a> </li> <li class="promo__item
+            promo__item--clothing"> <a class="promo__link" href="all-lots.html">Одежда</a> </li> <li class="promo__item promo__item--tools"> <a class="promo__link" href="all-lots.html">Инструменты</a> </li> <li class="promo__item promo__item--other"> <a
+            class="promo__link" href="all-lots.html">Разное</a> </li>-->
         </ul>
     </section>
     <section class="lots">
